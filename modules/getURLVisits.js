fd2767bc5d81d4dc331b3ba16046935cffb83ed7
Chad Garrett <chadgarrett_@hotmail.com>
2017-09-15 00:04:25 +0000

    Created function to return userIDs of Visitors in urlDatabase

---
+++ modules/getURLVisits.js
@@ -0,0 +1,12 @@
+module.exports = function getURLVisits(data, urlID) {
+    let urlVisits = {};
+    for (let urlIndex in data) {
+        if (data[urlIndex].shortURL === urlID) {
+            urlVisits[urlIndex] = data[urlIndex];
+        }
+    }
+    return urlVisits;
+}
+
+
+
